a8dd61d59227b7a9519a0a6dc75500f3aa14c235
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-05-24 16:19:41 +0530

    Initial Commit

---
+++ MoviesInfo/MoviesInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesInfoApp.swift
+//  MoviesInfo
+//
+//  Created by Asadullah Behlim on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MoviesInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
