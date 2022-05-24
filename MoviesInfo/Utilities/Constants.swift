6e6a70352a7f0aa86be6af03a4fe679be6b63133
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-05-24 16:40:54 +0530

    Created Folder Structure For Movies Info Application

---
+++ MoviesInfo/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  MoviesInfo
+//
+//  Created by Asadullah Behlim on 24/05/22.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "6051fee1" // OMDBAPI
+}
