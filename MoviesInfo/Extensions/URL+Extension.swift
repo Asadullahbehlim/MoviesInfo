d779f50a3bc7efaa6ae46c7910eb1f00f776a667
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-06-02 14:53:06 +0530

    URL Extension Updated

--- MoviesInfo/Extensions/URL+Extension.swift
+++ MoviesInfo/Extensions/URL+Extension.swift
@@ -9,8 +9,8 @@ import Foundation
 
 extension URL {
     
-    static func forMoviesByImdbId(_ name: String) -> URL? {
-        return URL(string: "http://omdbapi.com/?i=\(name)&apikey=\(Constants.API_KEY)")
+    static func forMoviesByImdbId(_ imdbId: String) -> URL? {
+        return URL(string: "http://omdbapi.com/?i=\(imdbId)&apikey=\(Constants.API_KEY)")
     }
     
     static func forMoviesByName(_ name: String) -> URL? {
